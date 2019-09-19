be20dbd9f37472653fee9850b77307ee716e6f33
Pavagravesket <dslpavi@gmail.com>
2019-09-19 21:49:50 +0530

    first commit

---
+++ Kata0.java
@@ -0,0 +1,13 @@
+package uk.ac.wlv.cs5006.kata0;
+
+
+public class Kata0 {
+	/**
+	 * @param args
+	 */
+	public int max(int input1,int input2){
+		
+		return ((input1)>(input2)?(input1):(input2));
+	
+	}
+}
